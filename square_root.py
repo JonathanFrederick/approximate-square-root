a769d7d3fcbe098b66a68e1ba554cccc429b8a7a
Jonathan Frederick <jrfreder@gmail.com>
2015-09-01 14:02:48 -0400

    Added prompt for number to  square_root.py

--- square_root.py
+++ square_root.py
@@ -1,1 +1,11 @@
 """A program that asks the user for a positive number and then outputs the approximated square root of the number."""
+while True:
+    try:
+        num = float(input('Please input a positive number:  '))
+        if num < 0:
+            num/0
+        break
+    except:
+        continue
+
+print(num)
