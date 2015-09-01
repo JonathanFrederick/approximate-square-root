6e6eb23f7de9f68db0dfdfc38ac8672db3b33ed7
Jonathan Frederick <jrfreder@gmail.com>
2015-09-01 14:39:08 -0400

    Added function to approximate sq rt to square_root.py

--- square_root.py
+++ square_root.py
@@ -8,4 +8,16 @@ while True:
     except:
         continue
 
+
 print(num)
+
+def sqrt (num):
+    """Approximates square root of parameter to 2 decimal places"""
+    guess1 = 1
+    guess2 = 0
+    while round(guess1, 2) != round(guess2, 2):
+        guess2 = guess1
+        guess1 = (guess1 + num/guess1)/2
+    return guess1
+
+print("The square root of", num, "is", round(sqrt(num),2))
