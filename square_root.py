b79a39ba1231fb4126b057fad5774a135ee0e717
Jonathan Frederick <jrfreder@gmail.com>
2015-09-01 14:48:00 -0400

    HARD MODE: added counter to loop, error message to input

--- square_root.py
+++ square_root.py
@@ -6,6 +6,7 @@ while True:
             num/0
         break
     except:
+        print("ERROR: Not valid input (not a positive number)")
         continue
 
 
@@ -15,9 +16,12 @@ def sqrt (num):
     """Approximates square root of parameter to 2 decimal places"""
     guess1 = 1
     guess2 = 0
+    count = 0
     while round(guess1, 2) != round(guess2, 2):
+        count += 1
         guess2 = guess1
         guess1 = (guess1 + num/guess1)/2
+        print("This loop has iterated", count, "times.")
     return guess1
 
 print("The square root of", num, "is", round(sqrt(num),2))
